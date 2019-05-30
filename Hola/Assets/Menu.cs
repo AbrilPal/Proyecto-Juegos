63811c4687dc8ff0b5afc7e67439d323d3ca38d7
unknown <pal18198@uvg.edu.gt>
2019-05-29 22:33:51 -0600

    Todo

--- Hola/Assets/Menu.cs
+++ Hola/Assets/Menu.cs
@@ -36,4 +36,8 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene("segundo");
     }
+    public void Tiro()
+    {
+        SceneManager.LoadScene("RelojNivel1");
+    }
 }
---
+++ Hola/Assets/Scripts/RelojNivel2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RelojNivel2 : MonoBehaviour
+{
+    public Personita script;
+    public Text TxtReloj;
+    private float countdown = 240;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        TxtReloj = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        TxtReloj.text = countdown.ToString("Faltan " + "0");
+        if (countdown <= 0)
+        {
+            if (script.puntuacion >= 20)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+            else if (script.puntuacion < 20)
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+
+        }
+    }
+}
